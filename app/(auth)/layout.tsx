6c1b8681c832eda6a1a2264529927539bd4b9ff9
Aurora Scharff <aurora.sofie@gmail.com>
2025-09-14 21:50:51 +0200

    Add notfound page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import AppLayout from '@/components/layout/AppLayout';
 import { getIsAuthenticated } from '@/features/auth/auth-queries';
 import { AuthProvider } from '@/features/auth/components/AuthProvider';
+import { ProductListSkeleton } from '@/features/product/components/ProductList';
 import UserProfile, { UserProfileSkeleton } from '@/features/user/components/UserProfile';
 
 export default async function AuthLayout({ children, modal }: LayoutProps<'/'>) {
@@ -10,8 +11,21 @@ export default async function AuthLayout({ children, modal }: LayoutProps<'/'>) 
   return (
     <AuthProvider loggedIn={loggedIn}>
       <AppLayout headerContent={<Suspense fallback={<UserProfileSkeleton />}>{<UserProfile />}</Suspense>}>
-        {children}
-        {modal}
+        <Suspense
+          fallback={
+            <>
+              <div className="skeleton-animation mb-4 h-30 w-full rounded-lg" />
+              <div className="skeleton-animation mb-4 h-10 w-full rounded-lg" />
+              <div className="flex h-full grow flex-col gap-4">
+                <div className="skeleton-animation h-6 w-24 rounded" />
+                <ProductListSkeleton />
+              </div>
+            </>
+          }
+        >
+          {children}
+          {modal}
+        </Suspense>
       </AppLayout>
     </AuthProvider>
   );
