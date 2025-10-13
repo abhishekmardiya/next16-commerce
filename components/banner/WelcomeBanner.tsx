51cade443ae10c35f8dc727a31f2eec3e84a51b7
Aurora Scharff <aurora.sofie@gmail.com>
2025-10-13 22:51:58 +0200

    Improve banner design

--- components/banner/WelcomeBanner.tsx
+++ components/banner/WelcomeBanner.tsx
@@ -14,21 +14,19 @@ export default function WelcomeBanner({ loggedIn }: { loggedIn: boolean }) {
 
   return (
     <Boundary>
-      <div className="border-divider dark:border-divider-dark from-accent/5 via-accent/3 dark:from-accent/10 dark:via-accent/5 relative border bg-gradient-to-tr to-transparent p-0 dark:to-transparent">
-        <div className="flex items-start justify-between gap-3 p-3 sm:gap-4 sm:p-5">
-          <div className="flex-1">
-            <PersonalBanner loggedIn={loggedIn} />
-          </div>
-          <button
-            onClick={() => {
-              setDismissed(true);
-            }}
-            className="group text-gray/70 hover:border-divider hover:text-accent dark:text-gray/60 dark:hover:text-accent -m-1 inline-flex h-6 w-6 items-center justify-center border border-transparent p-0 transition-colors"
-            aria-label="Dismiss banner"
-          >
-            <X aria-hidden className="h-4 w-4" />
-          </button>
+      <div className="border-divider dark:border-divider-dark from-accent/5 via-accent/3 dark:from-accent/10 dark:via-accent/5 relative flex items-start justify-between gap-3 border bg-gradient-to-tr to-transparent p-4 sm:gap-4 sm:p-5 dark:to-transparent">
+        <div className="flex-1">
+          <PersonalBanner loggedIn={loggedIn} />
         </div>
+        <button
+          onClick={() => {
+            setDismissed(true);
+          }}
+          className="group text-gray/70 hover:border-divider hover:text-accent dark:text-gray/60 dark:hover:text-accent -m-1 inline-flex h-6 w-6 items-center justify-center border border-transparent p-0 transition-colors"
+          aria-label="Dismiss banner"
+        >
+          <X aria-hidden className="h-4 w-4" />
+        </button>
       </div>
     </Boundary>
   );
