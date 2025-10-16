db638be427973b58624f444194e6505a07c35b1c
Aurora Scharff <aurora.sofie@gmail.com>
2025-10-16 17:31:08 +0200

    Undo cachecomponents config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    cacheComponents: true,
     clientSegmentCache: true,
     inlineCss: true,
     routerBFCache: true,
